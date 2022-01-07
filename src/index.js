f281c66e404cda5337ab78691c647e716ce441ff
Anderey Prokhorov <beta737@yandex.ru>
2022-01-07 20:23:25 +0300

    27 урок обязательное

--- src/index.js
+++ src/index.js
@@ -3,15 +3,57 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import calc from './modules/calc';
-import auth from './modules/auth';
+import formSend from './modules/formSend';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
 
 
-timer('2 january 2022');
+timer('17 january 2022');
 menu();
 modal();
 calc(100);
-auth();
 tabs();
 slider('.portfolio-item', '.portfolio-dots');
+formSend({
+    formId1: 'form1', 
+    someElement1: [{
+        type: 'block',
+        id: 'total'
+    }],
+},
+{
+    formId2: 'form2',
+    someElement2: [{
+        type: 'input',
+        id: 'total'
+    }],
+},
+{
+    formId3: 'form3',
+    someElement3: [{
+        type: 'block',
+        id: 'total'
+    }],
+});
+    
+    
+    
+    
+//     {
+//     formId1: 'form1',
+//     formId2: 'form2',
+//     formId3: 'form3',
+//     someElement1: [{
+//         type: 'block',
+//         id: 'total'
+//     }],
+//     someElement2: [{
+//         type: 'input',
+//         id: 'total'
+//     }],
+//     someElement3: [{
+//         type: 'block',
+//         id: 'total'
+//     }]
+  
+// });
---
+++ src/modules/formSend.js
@@ -0,0 +1,269 @@
+
+const formSend = ({formId1,someElement1 = []}, {formId2,someElement2 = []}, {formId3,someElement3 = []}) => {
+
+    const form1 = document.getElementById(formId1),
+        form2 = document.getElementById(formId2),
+        form3 = document.getElementById(formId3),
+        userNames = document.querySelectorAll('[name=user_name]'),
+        userEmails = document.querySelectorAll('[name=user_email]'),
+        userPhones = document.querySelectorAll('[name=user_phone]'),
+        userMessage = document.querySelector('[name=user_message]');
+       
+    const statusBlock = document.createElement('div'),
+         loadText = 'Загрузка...',
+         errorText = 'Ошибка...',
+         successText = 'Спасибо! Наш менеджер свами свяжется!';
+       
+       
+
+ 
+
+    const validEmail = (list) => {
+
+        list.forEach(email => {
+
+            email.addEventListener('blur', (e) => {
+                console.log(e.target.value);
+                let testEmail = /[a-z0-9~*!'_\-\.]*@[\w-]+\.\w{2,4}/gi;
+                if (testEmail.test(e.target.value)) {
+                    email.value = e.target.value;
+                } else {
+                    email.value = e.target.value.replace(/[а-яё]+/gi, '').replace(/@+/g, '@').replace(/^[\-\s]+/g, '').replace(/[\-\s]+$/g, '')
+
+                }
+
+
+            });
+        });
+    };
+
+    const validMessage = () => {
+
+        userMessage.addEventListener('input', (e) => {
+
+            e.target.value = e.target.value.match(/[а-яё0-9 \-\.,;:?!]+/gi);
+
+
+        });
+    };
+    
+    const validName = (list) => {
+        list.forEach(name => {
+            name.addEventListener('blur', (e) => {
+
+                let testText = /^[а-яё\- ]+/gi;
+
+                if (testText.test(e.target.value)) {
+                   
+                    name.value = e.target.value[0].toUpperCase() + e.target.value.slice(1);
+                } else {
+                    name.value = e.target.value.replace(/[^а-яё\- ]+/gi, '')[0].toUpperCase() + e.target.value.replace(/[^а-яё\- ]+/gi, '').slice(1);
+
+                }
+
+
+            });
+        });
+    };
+
+    const validPhone = (list) => {
+        list.forEach(phone => {
+            phone.addEventListener('blur', (e) => {
+                
+                let testPhone = /(\+7|8)[(]?(\d{3})[)]?(\d{3})[-]?(\d{2})[-]?(\d{2})/g;
+                console.log(testPhone.test(e.target.value));
+                if (testPhone.test(e.target.value)) {
+                   phone.value = e.target.value;
+                } 
+                else {
+                    phone.value = e.target.value.replace(/[а-яa-z]+/g, '').replace(/\-+/g, '-').replace(/[^\d\(\)\-\+]+/g, '').replace(/^[\-\s]+/gm, '').replace(/[\-\s]+$/gm, '');
+                }
+
+            });
+
+        });
+            
+    };
+
+            
+    // validEmail(userEmails);
+    // validMessage();
+    // validName(userNames);
+    // validPhone(userPhones);
+
+
+
+// Реализовать проверку введенных данных в поля ввода с помощью события blur(при потере полем ввода фокуса) и заменять их на корректные при необходимости по правилам:
+
+// Должны удаляться все символы, кроме допустимых
+// Несколько идущих подряд пробелов или дефисов должны заменяться на один.
+// Пробелы и дефисы в начале и конце значения должны удаляться.
+// Для поля type = text Первая буква каждого слова должна приводиться к верхнему регистру, а все остальные— к нижнему.
+
+
+
+   
+     
+    const validate = (list) => {
+        let success = false;
+        
+        const testPhone = /^(\+7|7|8)?[\-]?\(?[489][0-9]{2}\)?[\-]?[0-9]{3}[\-]?[0-9]{2}[\-]?[0-9]{2}$/;
+        const testName = /^[а-яё ]+$/i;
+        const testMessage = /[а-яё0-9 \-\.,;:?!]+/gi;
+        
+
+         let ttext;
+         let ttext1;
+
+         let eemail;
+         
+         let pphohe;
+         let pphone1;
+
+         let mmess;
+         let mmess1;
+         
+
+            
+      
+            list.forEach(input => {
+              
+                if (input.closest('[name=user_name]')) {
+                ttext = input.closest('[name=user_name]');
+                ttext = ttext.value;
+                }
+
+                if (input.closest('[name=user_phone]')) {
+                pphohe = input.closest('[name=user_phone]');
+                pphohe = pphohe.value;
+                }
+                if (input.closest('[name=user_email]')) {
+                    eemail = input.closest('[name=user_email]');
+                    eemail = eemail.value;
+                }
+                if (input.closest('[name=user_message]')) {
+                    mmess = input.closest('[name=user_message]');
+                    mmess = mmess.value;
+                }
+                // console.log(input.value)
+                // if (input.value == '') {
+                //     success = false;
+                    
+
+                  
+                // }
+               
+
+            });
+              console.log(eemail);
+              console.log(mmess);
+              console.log(ttext);
+              ttext1 = testName.test(ttext);
+              console.log(ttext1);
+              console.log(pphohe);
+              pphone1 = testPhone.test(pphohe);
+              console.log(pphone1);
+              console.log(ttext1 && pphone1);
+
+            
+            if (ttext1 && pphone1) {
+                success = true;
+                console.log('YES');
+
+            }
+             mmess1 = testMessage.test(mmess);
+             console.log(mmess1);
+
+            if (ttext == '' || eemail == '' || pphohe == '' || mmess == '') {
+                success = false;
+                console.log('OCHKO');
+                alert('Заполните все поля!!!');
+            }
+            if(mmess1) {
+                 success = true;
+                 console.log('YES MM1');
+            }
+
+
+    
+        return success;
+    };
+
+    const sendData = (data) => {
+        return fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+        }).then(res => res.json());
+    };
+
+    const submitForm = (form, someElement) => {
+        const formElements = form.querySelectorAll('input');
+        const formData = new FormData(form);
+        const formBody = {};
+
+        statusBlock.textContent = loadText;
+        form.append(statusBlock);
+
+
+
+        formData.forEach((val, key) => {
+            formBody[key] = val;
+        });
+        someElement.forEach(elem => {
+            const element = document.getElementById(elem.id);
+           
+
+            if (elem.type === 'block') {
+                formBody[elem.id] = element.textContent;
+            } else if (elem.type === 'input') {
+                formBody[elem.id] = element.value;
+            }
+
+        });
+
+        
+        if (validate(formElements)) {
+            sendData(formBody)
+                .then(data => {
+                    statusBlock.textContent = successText;
+                    
+                    formElements.forEach(input => {
+                        input.value = '';
+                    });
+                })
+                .catch(error => {
+                    statusBlock.textContent = errorText;
+                });
+
+        } else {
+            
+            
+            alert('Данные не валидны!!!!');
+        }
+    };
+    const formListener = (form, someElement) => {
+        try {
+            if (!form) {
+                throw new Error('Верните форму на место!!!');
+            }
+                form.addEventListener('submit', (e) => {
+                    e.preventDefault();
+
+                    submitForm(form, someElement);
+                    
+
+                });
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+    formListener(form1, someElement1);
+    formListener(form2, someElement2);
+    formListener(form3, someElement3);
+  
+};
+
+export default formSend;
